6a2c43bcd1e5dea7e7486c5ed3820ed26ddedbd9
yoojinsung <yoojinsung91@gmail.com>
2015-08-06 05:44:48 +0900

    Initial Calendar
    캘린더 초기버전 업데이트.

---
+++ APFAP/Project/PjtCom/ComFormB01.view.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../../Resource/Scripts/ext-all-debug.js" />
+/// <reference path="../../Resource/Scripts/component.js" />
+/// <reference path="../../Resource/Scripts/noncomponent.js" />
+//
+
+//View 단 정의 영역 시작
+Ext.require([
+    '*'
+]);
+
+Ext.onReady(function () {
+    
+});
