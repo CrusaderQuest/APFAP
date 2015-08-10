637c2b3adcfcca91b0f7c83c6ba5b5e4c7b70a5a
JuneJobs <j.jobs1028@gmail.com>
2015-08-10 17:40:29 +0900

    버튼이벤

--- APFAP/Project/TestCode/test.app.js
+++ APFAP/Project/TestCode/test.app.js
@@ -7,3 +7,17 @@
 //txt2.eChange = function (newValue, oldValue) {
 //    console.log(newValue);
 //}
+//체크버튼 클릭이벤트
+btn_aa.eClick = function () {
+    if (chk_aa.getValue()) {
+        chk_aa.setValue(false);
+    } else {
+        chk_aa.setValue(true);
+    }
+}
+btn_bb.eClick = function () {
+    tab.setActiveTab(1);
+}
+grd.eSelectionChange = function (record, rowIndex, dataIndex) {
+    console.log(record, rowIndex, dataIndex);
+}
