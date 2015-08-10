e297fd4ef3652b53de26e6bf723d4c8634e14c5a
PCE <pchangen@naver.com>
2015-08-11 01:52:19 +0900

    defD

--- APFAP/Project/PjtDef/DefFormD01.view.js
+++ APFAP/Project/PjtDef/DefFormD01.view.js
@@ -5,40 +5,15 @@
 
 //View 단 정의 영역 시작
 var pnl_contents = ApPanel.create("FUNC");
+var pnl_con = ApPanel.create("func");
+var pnl_vi = ApPanel.create("sd");
+var tt = ApTable.create();
+tt.setTarget();
+var bt = ApButton.create("sdd");
+var bte = ApButton.create("33");
 
-//data-type
-Ext.define('reqData', {
-    extend: 'Ext.data.Model',
-    fields: [
-        { name: 'USERID' },
-        { name: 'CATEGORY' },
-        { name: 'REQNM' },
-        { name: 'BLANK' }
-    ]
-});
+pnl_vi.divideV( pnl_con,pnl_contents);
+pnl_con.full(bte);
+pnl_contents.full(bt);
+viewPanel.full(pnl_vi);
 
-//pnl_grid
-var gridData = Ext.create('Ext.data.ArrayStore', {
-    model: 'reqData',
-    data: [
-        ['aaa1', '신규기능추가', '기존 ui와 차별화된 것을 만들어라'],
-        ['aaa2', '신규기능추가', '신규기능 추가하는 요구사항이다'],
-        ['aaa3', '구 기능 수정', '예전 ui를 차별화 되게 바꾸라'],
-        ['aaa4', '구 기능 수정', '구기능을 수정하는 요구사항이다.']
-    ]
-});
-
-var grd_a = ApGrid.create();
-
-grd_a.addColumn('text', '업무영역', 'CATEGORY', 200);
-grd_a.addColumn('text', '요구사항', 'REQNM', 500);
-grd_a.addColumn('text', '비고', 'BLANK', 300);
-grd_a.reconfigure(gridData);
-pnl_contents.full(grd_a);
-
-viewPanel.full(pnl_contents);
-
-
-Ext.onReady(function () {
-
-});
