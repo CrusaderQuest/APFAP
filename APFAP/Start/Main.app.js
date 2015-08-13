4b222a77c693b322cde3e29458f86aa9c2252e5c
JuneJobs <j.jobs1028@gmail.com>
2015-08-14 01:12:19 +0900

    메인폼, 디비커넥트, 라벨 모듈 추가가

---
+++ APFAP/Start/Main.app.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="../Resource/Script/ext-all-debug.js" />
+/// <reference path="../Resource/Script/component.js" />
+/// <reference path="../Resource/Script/noncomponent.js" />
+/// <reference path="Main.view.js" />
+
+//App 단 정의 영역 시작
+//폼 렌더링시 초기화
+function SYS_INIT() {
+    txt_TEAMNAME.setValue('Master Plan');
+}
