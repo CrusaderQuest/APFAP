0bd241f1dd227568ef9cb6a460290d8148da9e54
Yoojinsung <yoojinsung91@gmail.com>
2015-08-14 05:27:57 +0900

    DevFormA01
    업뎃이염

--- APFAP/Project/PjtDev/DevFormA01.view.js
+++ APFAP/Project/PjtDev/DevFormA01.view.js
@@ -5,37 +5,101 @@
 
 //View 단 정의 영역 시작
 
-var pnl_top = ApPanel.create("top field");
-var pnl_content = ApPanel.create("contents field");
+var currentBtn=0;
+//-------------------폼 내 모듈-시작----------------
+
+
+
+//-------------------폼 내 모듈 끝------------------
+var pnl_top = ApPanel.create();
+var pnl_content = ApPanel.create();
 
 viewPanel.divideV(pnl_top, pnl_content);
-pnl_top.setHeight('10%');
+pnl_top.setHeight(50);
 
-var pnl_title = ApPanel.create("개발 진척도");
+var tbl_top = ApTable.create(2);
+tbl_top.setTarget();
+var pnl_title = ApLabel.create("개발 진척도");
+pnl_title.setWidth(400);
 //pnl_title.setBodyStyle('background-color','#ff0000');
-var pnl_summary = ApPanel.create("개발 진행 상황에 대한 내역을 관리할 수 있습니다.");
-
-pnl_top.divideH(pnl_title, pnl_summary);
-pnl_title.setWidth('25%');
+var pnl_summary = ApLabel.create("개발 진행 상황에 대한 내역을 관리할 수 있습니다.");
+pnl_top.full(tbl_top);
 
 var pnl_graph = ApPanel.create("전체,개발자별 그래프 패널");
-var pnl_gridContent = ApPanel.create("탭,그리드 패널");
-var pnl_devTab = ApPanel.create("탭 패널");
-var pnl_devGrid = ApPanel.create("그리드 패널");
+var pnl_gridTab = ApPanel.create("탭(버튼),그리드 패널");
+pnl_content.divideV(pnl_graph, pnl_gridTab);
+pnl_graph.setHeight(100);
 
-pnl_content.divideV(pnl_graph, pnl_gridContent);
-pnl_gridContent.divideV(pnl_devTab, pnl_devGrid);
-pnl_devTab.setHeight('10%');
+var pnl_tabBtn = ApPanel.create("탭");
+var pnl_grd = ApPanel.create("그리드");
+pnl_gridTab.divideV(pnl_tabBtn, pnl_grd);
+pnl_tabBtn.setHeight(80);
 
-//--------------------------------------------------
-var comboStoreState = Ext.create('Ext.data.ArrayStore', {
-    fields: ['HIDEDATA', 'SHOWDATA'],
-    data: [
-        ['E01', '준비'],
-        ['E02', '진행 중'],
-        ['E03', '완료']
+var pnl_tab = ApPanel.create();
+var pnl_btn = ApPanel.create();
+pnl_tabBtn.divideV(pnl_tab, pnl_btn);
+
+var tbl_btn = ApTable.create(2);
+tbl_btn.setTarget();
+var btn_insert = ApButton.create("Add");
+var btn_delete = ApButton.create("Delete");
+tbl_btn.setPosition(1000, 0, null);
+pnl_btn.full(tbl_btn);
+
+//----------------------DB 데이터 포맷---------------
+//call GET_H_TABLE
+Ext.define('H_Data', {
+    extend: 'Ext.data.Model',
+    fields: [
+        { name: 'H_DEV_NO', type: 'int' },
+        { name: 'H_DEV_NM' }
     ]
 });
+var hData = Ext.create('Ext.data.ArrayStore', {
+    model: 'H_Data',
+    data: [
+        [0, '서버'],
+        [1, 'DB'],
+        [2, 'UI'],
+        [3, 'ETC']
+    ]
+});
+//hData의 H_DEV_NO 에 접근해서 dData를 가져옴. USER 랑 조인.
+//call GET_D_TABLE
+Ext.define('D_Data', {
+    extend: 'Ext.data.Model',
+    fields: [
+        { name: 'D_DEV_NM' },
+        { name: 'START_DT', type: 'date', dateFormat: 'Ymd' },
+        { name: 'DEV_VALUE' },
+        { name: 'TEST_VALUE' },
+        { name: 'DEADLINE', type: 'date', dateFormat: 'Ymd'},
+        { name: 'WORKER' },
+        { name: 'END_DT', type: 'date', dateFormat: 'Ymd' }
+    ]
+});
+var dData = Ext.create('Ext.data.ArrayStore', {
+    model: 'D_Data',
+    data: [
+        ['서버 모듈 개발1', '20150702', 'T', 'T', '20150812', '거스', '20150810'],
+        ['서버 모듈 개발2', '20150702', 'T', 'F', '20150812', '거스', ''],
+        ['서버 모듈 개발3', '20150702', 'F', 'F', '20150812', '거스', '']
+    ]
+});
+var sampleData = Ext.create('Ext.data.ArrayStore', {
+    model: 'D_Data',
+    data: [
+        ['샘플1', '20150702', 'T', 'T', '20150812', '거스', '20150810'],
+        ['샘플2', '20150702', 'T', 'F', '20150812', '거스', ''],
+        ['샘플3', '20150702', 'F', 'F', '20150812', '거스', '']
+    ]
+});
+
+//----------------------DB 데이터 포맷 끝------------------
+
+//------------------------그리드 시작----------------------
+
+//PROJECT_USER 에서 프로젝트에 속한 유저들 가져와야함.
 var comboStoreWorker = Ext.create('Ext.data.ArrayStore', {
     fields: ['HIDEDATA', 'SHOWDATA'],
     data: [
@@ -46,34 +110,35 @@ var comboStoreWorker = Ext.create('Ext.data.ArrayStore', {
         ['U0000005', '기니니']
     ]
 });
-
-Ext.define('devData', {
-    extend: 'Ext.data.Model',
-    fields: [
-        { name: 'DEVNM' },
-        { name: 'DEVSTATE' },
-        { name: 'TESTSTATE' },
-        { name: 'S_DT', type: 'date', dateFormat: 'Y-m-d' },
-        { name: 'DEADLINE', type: 'date', dateFormat: 'Y-m-d' },
-        { name: 'WORKER' }
-    ]
-});
-var gridData = Ext.create('Ext.data.ArrayStore', {
-    model: 'devData',
+//DEV_VALUE, TEST_VALUE true,false 값 콤보박스.
+var comboStoreValue = Ext.create('Ext.data.ArrayStore', {
+    fields: ['HIDEDATA', 'SHOWDATA'],
     data: [
-        ['메인 UI', '진행 중', '준비', '2015-07-02', '2015-07-03', '거니니'],
-        ['로그인 UI', '준비', '준비', '2015-07-03', '2015-07-04', '기니니'],
-        ['공통 UI', '완료', '진행 중', '2015-07-04', '2015-07-05', '으니니']
+        ['true', 'T'],
+        ['false', 'F']
     ]
 });
+
 var grd = ApGrid.create();
-grd.addColumn('text', '개발 단위', 'DEVNM', 200);
-grd.addColumn('combo', '개발 상태', ['DEVSTATE', comboStoreState], 120);
-grd.addColumn('combo', '테스트 상태', ['TESTSTATE', comboStoreState], 120);
-grd.addColumn('date', '시작날짜', 'S_DT', 120);
+grd.addColumn('text', '개발 단위', 'D_DEV_NM', 200);
+grd.addColumn('date', '시작 날짜', 'START_DT', 120);
+grd.addColumn('combo', '개발 상태', ['DEV_VALUE', comboStoreValue], 120);
+grd.addColumn('combo', '테스트 상태', ['TEST_VALUE', comboStoreValue], 120);
 grd.addColumn('date', '데드라인', 'DEADLINE', 120);
 grd.addColumn('combo', '담당자', ['WORKER', comboStoreWorker], 120);
+grd.addColumn('date', '완료 날짜', 'END_DT', 120);
 
-grd.reconfigure(gridData);
-//--------------------------------------------------
-pnl_devGrid.full(grd);
+grd.reconfigure(dData);
+
+pnl_grd.full(grd);
+//--------------------그리드 끝---------------------
+//-------------------탭 (버튼) 시작-----------------
+var tab = ApTable.create(4);
+tab.setTarget();
+var btn_server = ApButton.create("서버");
+var btn_db = ApButton.create("DB");
+var btn_ui = ApButton.create("UI");
+var btn_etc = ApButton.create("기타");
+pnl_tab.full(tab);
+
+//-------------------탭 (버튼) 끝-------------------
