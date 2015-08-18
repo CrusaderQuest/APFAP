790b87b0ce5e1c50b002f80807b61b451ce147b8
PCE <pchangen@naver.com>
2015-08-18 15:00:07 +0900

    defA up

--- APFAP/Project/PjtDef/DefFormA01.app.js
+++ APFAP/Project/PjtDef/DefFormA01.app.js
@@ -5,11 +5,24 @@
 
 //App 단 정의 영역 시작
 
-//function GRD_LOAD() {
-//    //데이터생성
-//    var pr = DBParams.create('sp_ANLFORMA01', 'GET_TABLE');
-//    //데이터셋
-//    var ds = DBconnect.runProcedure(pr);
-//    gridData = ds[0];
-//    grd_a.reconfigure(gridData);
-//}
+function GRD_LOAD() {
+    //데이터생성
+    //var pr = DBParams.create('sp_ANLFORMA01', 'GET_TABLE');
+    ////데이터셋
+    //var ds = DBconnect.runProcedure(pr);
+    //gridData = ds[0];
+    //grd_a.reconfigure(gridData);
+}
+
+btn_change.eClick = function () {
+    txt_title.setDisabled(false);
+    txta_subtitle.setDisabled(false);
+    btn_change.setVisible(false)
+    btn_change2.setVisible(true);
+}
+btn_change2.eClick = function () {
+    txt_title.setDisabled(true);
+    txta_subtitle.setDisabled(true);
+    btn_change2.setVisible(false);
+    btn_change.setVisible(true);
+}
