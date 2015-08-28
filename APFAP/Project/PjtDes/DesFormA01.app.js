5d302ee10bfe77d6d78919ced49543cfc655c793
SeungHyeok <tmdgur52@naver.com>
2015-08-28 17:17:51 +0900

    DesA,B약간수정
    DesA,B약간수정

--- APFAP/Project/PjtDes/DesFormA01.app.js
+++ APFAP/Project/PjtDes/DesFormA01.app.js
@@ -12,3 +12,16 @@
 //btn_delete.eClick = function () {
 //    Ext.Msg.alert("창 이름", "경고 내용");
 //}
+function getTable() {
+    var prm = DBParams.create('sp_DesFormA01', 'GET_TABLE');
+    var dbc = DBconnect.runProcedure(prm);
+    dbc_save = dbc[0];
+    //var pn = DBParams.create('sp_DesFormB01', 'GET_TABLE_FOR_TREE');
+    //var dtree = DBconnect.runProcedure(pn);
+    //tree_save = dtree[0];
+    grd.reconfigure(dbc_save);
+    //var prm2 = DBParams.create('sp_DesFormB01', 'GET_DEFAULT_COLUMN');
+    //var column_detail = DBconnect.runProcedure(prm2);
+    //tbldetail_save = column_detail[0];
+    //grd_Detail.reconfigure(tbldetail_save);
+}
