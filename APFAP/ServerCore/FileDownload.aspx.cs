6eb5d0aa7ea7811646333a93a307eb7ad6197bad
JuneJobs <j.jobs1028@gmail.com>
2015-09-06 22:45:20 +0900

    파일 업로더 이미지 업로더 추가 파일업로더 ApUpload 함수 - create('텍스트', '파라메터 아무거나') : 생성자 이벤트 - getFileKey() : 파일키 가져옴 - getFileName() : 파일이름 가져옴(그리드 보여주기용?) - setFileKey(파일키) : 서버에 저장된 파일키를 기반으로 컴포넌트를 설정할 수 있음 - eUpload(파일키) : 업로드 버튼클릭시 발생하는 이벤트, 서버에 갔다와서 파일키를 리턴한다. - eClear() : 삭제버튼 클릭시 발생하는 이벤트 이미지 뷰어 함수 - create('소스경로', 파라메터아무거나) :생성자 이벤트  처음 생성할때 ''으로 경로보내주면됨 - setFileKey(파일키) : 파일키를 입력하면 서버에서 해당 이미지를 가져와서 영역을 채워줌(''을 넘기면 초기화됨) 예제소스는 Project/TestCode/test.view 의 동작을 확인하면 될 것임 *주의사항 : 로컬에서 파일업로드를 할경우 UploadFiles라는 폴더에 파일이 생성될 건데, 그거는 커밋할때 제외하셍♥

---
+++ APFAP/ServerCore/FileDownload.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Data;
+
+namespace APFAP.ServerCore
+{
+    public partial class FileDownload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string fileKey = "";
+            string fileName = "";
+            string path = "";
+            fileKey = Request["FILEKEY"];
+            //DB통신
+            string dbServer = "ms1201.gabiadb.com";
+            string dbPort = "1433";
+            string dbName = "apfap";
+            string dbUser = "masterplan";
+            string dbPass = "apfap1234[]";
+            string connectStr = "";
+            SqlCommand cmd = new SqlCommand();
+            StringBuilder BuildStr = new StringBuilder();
+
+            SqlParameter param1 = new SqlParameter();
+            SqlParameter param2 = new SqlParameter();
+            SqlParameter param3 = new SqlParameter();
+            param1.ParameterName = "@WORKGB";
+            param1.Value = "GETFILE";
+            param1.SqlDbType = SqlDbType.NVarChar;
+            cmd.Parameters.Add(param1);
+            param2.ParameterName = "@PROJECT_KEY";
+            param2.Value = Request["PROJECT_KEY"];
+            param2.SqlDbType = SqlDbType.NVarChar;
+            cmd.Parameters.Add(param2);
+            param3.ParameterName = "@FILEKEY";
+            param3.Value = Request["FILEKEY"];
+            param3.SqlDbType = SqlDbType.NVarChar;
+            cmd.Parameters.Add(param3); 
+
+            BuildStr.Append("Data Source=" + dbServer + "," + dbPort + ";");
+            BuildStr.Append("Initial Catalog=" + dbName + ";");
+            BuildStr.Append("User ID=" + dbUser + ";");
+            BuildStr.Append("Password=" + dbPass + ";");
+            connectStr = BuildStr.ToString();
+
+            SqlConnection conn = new SqlConnection(connectStr);
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_COMFILE";
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            conn.Open();
+            DataSet ds = new DataSet();
+            adapter.SelectCommand = cmd;
+            adapter.Fill(ds);
+            conn.Close();
+            DataTable dt = ds.Tables[0];
+            if (ds != null)
+            {
+                if (ds.Tables[0].Rows.Count > 0) {
+                    fileName = @ds.Tables[0].Rows[0][0].ToString() + @ds.Tables[0].Rows[0][1].ToString();
+                    path = HttpContext.Current.Server.MapPath(".").Split(new string[] { @"\ServerCore" }, StringSplitOptions.None)[0] + string.Format(@"\UploadFiles\{0}\{1}", Request["E_USER"], ds.Tables[0].Rows[0][1].ToString());
+
+                    Response.Clear();
+                    Response.AddHeader("Content-Disposition", "attachment; filename=" + HttpUtility.UrlEncode(@ds.Tables[0].Rows[0][1].ToString(), System.Text.Encoding.GetEncoding("utf-8")).Replace("+", " "));
+                    Response.TransmitFile(path);
+                    Response.End();
+                }           
+            }
+        }
+    }
+}
