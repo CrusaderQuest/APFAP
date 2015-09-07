065ff42bd65d5ca5e49991562435b02bbb274509
PCE <pchangen@naver.com>
2015-09-07 11:43:34 +0900

    temp

---
+++ APFAP/Project/PjtLogin/Login.view.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="../../Resource/Scripts/ext-all-debug.js" />
+/// <reference path="../../Resource/Scripts/component.js" />
+/// <reference path="../../Resource/Scripts/noncomponent.js" />
+/// <reference path="Login.app.js" />
+
+//panel
+var pnl_contents = ApPanel.create();
+var pnl_login = ApPanel.create();
+
+//data-type
+
+// tbl_login
+var tbl_login = ApTable.create();
+tbl_login.addCls('tableStyle_main');
+tbl_login.updateLayout();
+tbl_login.setTarget();
+var btn_save = ApButton.create("변경상태 저장");
