2d831951ff9a7bfd8a6afe1ea10c33b9d8a4d904
SeungHyeok <tmdgur52@naver.com>
2015-10-04 02:06:38 +0900

    주석 끝
    주석끝

--- APFAP/Project/PjtDes/DesFormB01.app.js
+++ APFAP/Project/PjtDes/DesFormB01.app.js
@@ -5,14 +5,12 @@
 
 //App 단 정의 영역 시작
 
-//eSelectionChange(a,b,c);
 
-
-grd.eSelectionChange = function (record, rowindex, paramId) {
-    if (isUpdated) {
-        msgSaveWarning(record);
+grd.eSelectionChange = function (record, rowindex, paramId) {//왼쪽 grid 클릭시 발생하는 이벤트
+    if (isUpdated) {//isUpdate 라는 전역변수가 0이 아니면 
+        msgSaveWarning(record);//메시지 호출을 위한 msgSaveWarning 함수 호출
     } else {
-        if (record.data.TABLE_KEY == 0) {
+        if (record.data.TABLE_KEY == 0) {//db에서 data 불러오는 부분
             pr = DBParams.create('sp_DesFormB01', 'ALLOC_H_TABLE');
             ds = DBconnect.runProcedure(pr);
             currentTable = ds[0].data.items[0].data.TABLE_KEY;
@@ -20,15 +18,15 @@ grd.eSelectionChange = function (record, rowindex, paramId) {
         } else {
             currentTable = record.data.TABLE_KEY;
         }
-        viewTable(currentTable);
+        viewTable(currentTable);//viewTable함수 호출
     }
 }
-function viewTable(key) {
+function viewTable(key) {//init Column함수 호출 , getColumn 함수호출 , getExample함수호출
     init_Column(key);
     getColumn(key);
     getExample(key);
 }
-function init_Table() {
+function init_Table() {//왼쪽 table 을 초기화 시켜주는 함수
     var prm = DBParams.create('sp_DesFormB01', 'GET_TABLE_NM');
     var tblnm = DBconnect.runProcedure(prm);
     if (tblnm[0].data.length == 0)
@@ -37,7 +35,7 @@ function init_Table() {
         tblnm = DBconnect.runProcedure(prm);
     }
 }
-function init_Column(key) {
+function init_Column(key) {//column 들을 초기화 시켜주는 함수
     var prm = DBParams.create('sp_DesFormB01', 'GET_COLUMN');
     prm.addParam('TABLE_KEY',key);
     var tblnm = DBconnect.runProcedure(prm);
@@ -47,21 +45,20 @@ function init_Column(key) {
         tblnm = DBconnect.runProcedure(prm);
     }
 }
-function getTable(key) {
+function getTable(key) {//DB에 저장된 table 을받아오는 함수
     var prm = DBParams.create('sp_DesFormB01', 'GET_TABLE_NM');
     var tblnm = DBconnect.runProcedure(prm);
     tblnm_save = tblnm[0];
     grd.reconfigure(tblnm_save);
-    //if (key == 0) currentTable = tblnm_save.data.items[0].data.TABLE_KEY;
 }
-function getColumn(key) {
+function getColumn(key) {//DB에 저장된 column을 받아오는 함수
     var prm2 = DBParams.create('sp_DesFormB01', 'GET_COLUMN');
     prm2.addParam('TABLE_KEY', key);
     var column_detail = DBconnect.runProcedure(prm2);
     tbldetail_save = column_detail[0];
     grd_Detail.reconfigure(tbldetail_save);
 }
-function getExample(key) {//
+function getExample(key) {//DB에 저장된 column별 example을 불러옵니다
     var prm3 = DBParams.create('sp_DesFormB01', 'GET_EXAMPLE');
     prm3.addParam('TABLE_KEY', key);
     var column_ex = DBconnect.runProcedure(prm3);
@@ -69,41 +66,40 @@ function getExample(key) {//
     grd_Example.reconfigure(example_save);
     //prm3.addParam('COLUMN_NO', tbldetail_save.data.items[0].data.COLUMN_NO); 
 }
-grd.eButtonAddClick = function () {
+grd.eButtonAddClick = function () {//grid 위에 추가 버튼을 누르면 한 행을 추가합니다.
     grd.addRow();
 }
-grd.eButtonDeleteClick = function () {
+grd.eButtonDeleteClick = function () {//grid 위에 삭제버튼을 누르면 한 행을 삭제합니다.
     var selectedRecords = grd.getSelectedRecords();
     var index = grd.getRowIndex(grd.getSelectedRecords()[0]);
     grd.deleteRow(selectedRecords);
     grd.setFocus(index - 1);
 }
-grd_Detail.eButtonAddClick = function () {
+grd_Detail.eButtonAddClick = function () {//column 부분의 grid 위에 추가버튼 누르면 한행 추가 
     grd_Detail.addRow();
     isUpdated = 1;
 }
-grd_Detail.eButtonDeleteClick = function () {
+grd_Detail.eButtonDeleteClick = function () {//column 부분의 grid 위에 삭제버튼 누르면 한행 삭제 
     var selectedRecords = grd_Detail.getSelectedRecords();
     var index = grd_Detail.getRowIndex(grd_Detail.getSelectedRecords()[0]);
     grd_Detail.deleteRow(selectedRecords);
     grd_Detail.setFocus(index - 1);
     isUpdated = 1;
 }
-grd_Detail.eUpdate = function (record, rowIndex, paramId) {
+grd_Detail.eUpdate = function (record, rowIndex, paramId) {//isUpdated전역변수를 1로 설정
     isUpdated = 1;
 }
 grd_Example.eUpdate = function (record, rowIndex, paramId) {
     isUpdated = 1;
 }
-btn_SAVE.eClick = function () {
+btn_SAVE.eClick = function () {//sync버튼 클릭시 dbSave()호출
     dbSave();
 }
 function dbSave() {
-    //if (!isUpdated) return;
 
-    var selectedRecords = grd.getSelectedRecords();
+    var selectedRecords = grd.getSelectedRecords();//선택된 행을 변수에 할당
 
-    for (var i = 0; i < selectedRecords.length; i++) {
+    for (var i = 0; i < selectedRecords.length; i++) {//table 부분의 update 를 위한 과정
         var pr = '';
         if (selectedRecords[i].get('TABLE_KEY') == undefined || selectedRecords[i].get('TABLE_KEY') == 0) {
             pr = DBParams.create('sp_DesFormB01', 'INSERT_H_TABLE');
@@ -118,7 +114,7 @@ function dbSave() {
 
     selectedRecords = grd_Detail.getSelectedRecords();
 
-    for (var i = 0; i < selectedRecords.length; i++) {
+    for (var i = 0; i < selectedRecords.length; i++) {//column grid부분의 update를 위한 과정
         var pr = '';
         if (selectedRecords[i].get('COLUMN_NO') == undefined || selectedRecords[i].get('COLUMN_NO') == 0) {
             pr = DBParams.create('sp_DesFormB01', 'INSERT_D_TABLE');
@@ -135,7 +131,7 @@ function dbSave() {
         var dbc = DBconnect.runProcedure(pr);
     }
 
-    for (var i = 0; i < example_save.data.length; i++) {
+    for (var i = 0; i < example_save.data.length; i++) {//example 부분의 grid update 를 위한 과정
         var pr = '';
         pr = DBParams.create('sp_DesFormB01', 'UPDATE_EX_TABLE');
         pr.addParam('COLUMN_NO', example_save.data.items[i].get('COLUMN_NO'));
@@ -148,7 +144,7 @@ function dbSave() {
     }
 
     var deletedRecords = grd.getDeletedRecords();
-    for (var i = 0; i < deletedRecords.length; i++) {
+    for (var i = 0; i < deletedRecords.length; i++) {//행 삭제버튼을 누른 후 table update과정
         if (deletedRecords[i].get('TABLE_KEY') != undefined || deletedRecords[i].get('TABLE_KEY') != 0) {
             pr = DBParams.create('sp_DesFormB01', 'DELETE_H_TABLE');
             pr.addParam('TABLE_KEY', deletedRecords[i].get('TABLE_KEY'));
@@ -156,7 +152,7 @@ function dbSave() {
         }
     }
     deletedRecords = grd_Detail.getDeletedRecords();
-    for (var i = 0; i < deletedRecords.length; i++) {
+    for (var i = 0; i < deletedRecords.length; i++) {//행 삭제 후 column update 과정
         if (deletedRecords[i].get('COLUMN_NO') != undefined || deletedRecords[i].get('COLUMN_NO') != 0) {
             pr = DBParams.create('sp_DesFormB01', 'DELETE_D_TABLE');
             pr.addParam('COLUMN_NO', deletedRecords[i].get('COLUMN_NO'));
@@ -169,7 +165,7 @@ function dbSave() {
 
     isUpdated = 0;
 }
-
+//클릭하지 않고 저장시 변경사항 저장을 위한 msgSaveWarning (메시지를 출력하고 저장여부를 물어봄 )
 function msgSaveWarning(record) {
     Ext.Msg.show({
         message: '저장되지 않은 데이터는 삭제될 수 있습니다.\n저장 하시겠습니까?',
--- APFAP/Project/PjtDes/DesFormB01.view.js
+++ APFAP/Project/PjtDes/DesFormB01.view.js
@@ -6,20 +6,21 @@
 //View 단 정의 영역 시작
 //data Base 설계 부분 
 
-var isUpdated = 0;
+var isUpdated = 0;//update 를 위한 전역변수
 var currentTable = 0;
 
 var pnl_DBname = ApPanel.create('table Name');
 
-var grd = ApGrid.create(true, true);
+var grd = ApGrid.create(true, true);//grid 추가 table 이름을 column 으로 가지는 grid
 grd.addColumn('text', 'table Name', 'TABLE_NM', 195);
+//panel 선언
 var pnl_DBdetail_up = ApPanel.create('Up of up on grid');
 var pnl_DBdetail = ApPanel.create('table Detail');
 
 var pnl_DBdetail_OUT = ApPanel.create('Up');
 var tbl_grd = ApTable.create(1);
 tbl_grd.setTarget();
-var grd_Detail = ApGrid.create(true, true);
+var grd_Detail = ApGrid.create(true, true);//column 의 detail사항들을 기록하기위한 grid 선언 column의 이름, data type , primary key 설정 , null여부 설정
 grd_Detail.addColumn('text', 'column Name', 'COLUMN_NM', 200);
 grd_Detail.addColumn('text', 'data Type', 'DATA_TYPE', 200);
 grd_Detail.addColumn('check', 'primary Key', 'PRIMARY_CHECK', 200);
@@ -31,31 +32,26 @@ pnl_DBdetail_up.full(tbl_grd);
 tbl_grd.setWidth('fit');
 tbl_grd.setHeight(500);
 
-var grd_Example = ApGrid.create();
+var grd_Example = ApGrid.create();//column별 example 을 위한 grid 
 grd_Example.addColumn('text', 'column Name', 'COLUMN_NM', 150);
 grd_Example.addColumn('text', 'for example1', 'EXAMPLE_ONE', 150);
 grd_Example.addColumn('text', 'for example2', 'EXAMPLE_TWO', 150);
 grd_Example.addColumn('text', 'for example3', 'EXAMPLE_THREE', 150);
 grd_Example.addColumn('text', 'for example4', 'EXAMPLE_FOUR', 150);
 
-var tblnm_save;
-var tbldetail_save;
-var example_save;
+var tblnm_save;//table_name 을 위한 전역변수
+var tbldetail_save;//column들을 위한 전역변수
+var example_save;//column 별 example 을 위한 전역변수
 ///////////////////////////////////////////////////////////////////////////////////
 
 var pnl_search = ApPanel.create('search');
-
-;
 var pnl_main = ApPanel.create('main');
 //////////////////////////////////////////////////////////////////////////////////
 
 
-ApEvent.onlaod = function () {
+ApEvent.onlaod = function () {//panel 배치 및 init table 과 gettable 함수 호출
 
     pnl_main.divideH(pnl_DBname, pnl_DBdetail);
-
-
-
     pnl_DBdetail_OUT.full(pnl_DBdetail_up);
     pnl_DBdetail_up.setWidth('fit');
     pnl_DBdetail.divideV(pnl_DBdetail_OUT, grd_Example, pnl_DBdetail_OUT);
@@ -65,8 +61,6 @@ ApEvent.onlaod = function () {
     pnl_DBname.setWidth(250);
     init_Table();
     getTable(0);
-    //getColumn(currentTable);
-    //getExample(currentTable);
     viewPanel.full(pnl_main);
 
     grd_Example.setLockColumns('COLUMN_NM');
